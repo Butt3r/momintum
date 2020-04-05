94058557f17b78459d62ab35a4cf1fe88dddbe06
Song <oos9596@gmail.com>
2020-04-05 17:53:12 +0900

    updated storage system

---
+++ greeting.js
@@ -0,0 +1,66 @@
+const form = document.querySelector(".js-form"),
+      input = form.querySelector("input"),
+      greeting = document.querySelector(".js-greetings");
+      
+
+
+const USER_LIST = "currentUser",
+      SHOWING_NAME = "showing";
+
+
+
+function saveName(text)
+{
+  localStorage.setItem(USER_LIST, text);
+}
+
+
+function setName()
+{
+    form.classList.add(SHOWING_NAME);
+    form.addEventListener("submit",  submitHandeler);
+
+}
+
+
+function getName()
+{
+    const currentUser = localStorage.getItem(USER_LIST);
+    if(!currentUser)
+    {
+        console.log("input any user name");
+        setName();
+        
+    }
+    else
+    {
+        printGreeting(currentUser);
+    }
+}
+
+function printGreeting(text)
+{
+    form.classList.remove(SHOWING_NAME);
+    greeting.classList.add(SHOWING_NAME);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function submitHandeler(event)
+{
+    event.preventDefault();
+    const currentVal = input.value;
+    console.log(currentVal);
+    printGreeting(currentVal);
+    saveName(currentVal);
+    
+}
+
+
+
+
+function init()
+{
+    getName();
+}
+
+init();
